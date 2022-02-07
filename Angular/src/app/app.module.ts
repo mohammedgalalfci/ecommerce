b8cbb7b2f1f371df7fd8e70cf438edc870de61ae
nada <nada.h22425@gmsil.com>
2022-02-07 23:36:56 +0200

    add client side

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+import { WomenComponent } from './women/women.component';
+import { KidsComponent } from './kids/kids.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsUsComponent } from './contacts-us/contacts-us.component';
+
+import { AcountComponent } from './acount/acount.component';
+import { MainhomeComponent } from './mainhome/mainhome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    WomenComponent,
+    KidsComponent,
+    AboutComponent,
+    ContactsUsComponent,
+    AcountComponent,
+    MainhomeComponent
   ],
   imports: [
     BrowserModule,
