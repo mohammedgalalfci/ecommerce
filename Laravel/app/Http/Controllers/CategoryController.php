e135ba8efcb2b165d8f1f101ed6ff1d361a1363a
mohammedgalalfci <mohammedgalalfci@gmail.com>
2022-02-08 08:34:26 -0500

    added resources

--- Laravel/app/Http/Controllers/CategoryController.php
+++ Laravel/app/Http/Controllers/CategoryController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+        $categories=Category::all();  
+        return CategoryResource::collection($categories);
+    }
+    public function store(){
+        $data = request()->all();
+        $category=Category::create([
+            'cat_name' => $data['cat_name'],
+        ]);
+        return new CategoryResource($category);
+    }
+    public function show($category){
+        $oneCategory=Category::findOrFail($category);
+        return new CategoryResource($oneCategory);
+    }
+    public function update($category,Request $req){
+        $oneCategory=Category::findOrFail($category);
+        $oneCategory->update([
+            'cat_name' => $req['cat_name'],
+        ]);
+        return $oneCategory;
+    }
+
+    public function delete($category){
+        $oneCategory=Category::findOrFail($category);
+        $oneCategory->delete();
+        return new CategoryResource($oneCategory);
+    }
 }
--- Laravel/routes/api.php
+++ Laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/categories',[CategoryController::class,'index']);
+Route::post('/categories',[CategoryController::class,'store']);
+Route::get('/categories/{category}',[CategoryController::class,'show']);
+Route::put('/categories/{category}',[CategoryController::class,'update']);
+Route::delete('/categories/{category}',[CategoryController::class,'delete']);
